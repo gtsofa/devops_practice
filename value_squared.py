3269aa4ce62dc5c2942a4acec40c84e494a92037
Julius Nyule <jlsnyule@yahoo.com>
2018-04-19 07:53:34 +0300

    add value_squared.py

---
+++ value_squared.py
@@ -0,0 +1,23 @@
+'''Let's think about the steps needed to 
+write a function that returns a number's squared value
+'''
+#understand the problem
+#solve problem manually
+#make manual solution better
+#write pseudo code 
+#replace pseudo code w real code
+
+#solution
+#initialize number by b 
+#multiply number b by itself
+#display multiplication results
+
+def number_squared(b):
+  """returns a number's squared value"""
+  return b**2 
+  
+#testing
+test1 = number_squared(4)
+print(test1)
+  
+
